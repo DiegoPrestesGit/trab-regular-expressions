50d4cc94ea870f0b0ea2a14407c0bf3dfedf071d
Diego Prestes <diegoprestes.git@hotmail.com>
2021-06-12 16:15:30 -0300

    style: g

--- exercises.js
+++ exercises.js
@@ -61,10 +61,10 @@ const g = () => {
   if (expression.length === 0) return 'não tem nada pra validar aqui!'
   if (arr.some(letter => letter !== '1' && letter !== '0')) return 'cadeia só deve ter 0s e 1s'
 
-  return arr[0] == 0 && 
-  arr[1] == 1 && 
-  arr[arr.length-2] == 1 && 
-  arr[arr.length-1] == 0 ? 'cadeia valida' : 'cadeia invalida'
+  return arr[0] == 0 &&
+    arr[1] == 1 &&
+    arr[arr.length - 2] == 1 &&
+    arr[arr.length - 1] == 0 ? 'cadeia valida' : 'cadeia invalida'
 }
 
 // h - todas as cadeias que contenham exatamente quatro 1s;
@@ -73,6 +73,8 @@ const h = () => {
   const arr = expression.split('')
   if (expression.length === 0) return 'não tem nada pra validar aqui!'
   if (arr.some(letter => letter !== '1' && letter !== '0')) return 'cadeia só deve ter 0s e 1s'
+
+  
 }
 
 console.log(g())
